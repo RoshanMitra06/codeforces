a0c4cf24322f698af6b85ab81c83379aae84008a
Roshan <roosh.06@gmail.com>
2020-09-25 13:03:00 +0530

    Add solution for problem `B - Rock and Lever` Link: https://codeforces.com/contest/1420/problem/B Tags: bitmasks, math, *1200 Ref: https://codeforces.com/contest/1420/submission/93760890

---
+++ codeforces/1420/B.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//keep coding
+//-Roshan Mitra (SIT Pune) 
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define f0(i,n) for(int i=0;i<n;i++) 
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define int long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+signed main()
+{
+
+FIO; 
+int t;
+cin>>t;
+while(t--){
+ int n;
+ cin>>n;
+ vi v(n);
+ f0(i,n)
+    cin>>v[i];
+int total=0;
+vector<bool>visited(n,false);
+for(int i=31;i>=0;i--){
+    int num=0;
+    for(int j=0;j<n;j++){
+        if(!visited[j]&&(1<<i&v[j])){
+            num++;
+            visited[j]=true;
+        }
+    }
+    total+=num*(num-1)/2;
+}
+ cout<<total<<endl;
+} 
+return 0;
+   
+}
