b47c289f99f35f6e1bf0b0b1112a4f1b0b332c05
Roshan <roosh.06@gmail.com>
2020-11-07 12:48:00 +0530

    Add solution for problem `A - Yet Another Two Integers Problem` Link: https://codeforces.com/contest/1409/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1409/submission/97825054

---
+++ codeforces/1409/A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//keep coding
+//-Roshan Mitra (SIT Pune) 
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define f0(i,n) for(int i=0;i<n;i++) 
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define int long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+signed main()
+{
+
+FIO;
+int t;
+cin>>t;
+while(t--){
+ int a,b;
+ cin>>a>>b;
+ int x=abs(a-b);
+ int ans=x/10;
+ if(x%10!=0)
+    ans++;
+cout<<ans<<endl;
+ 
+} 
+return 0;
+   
+}
