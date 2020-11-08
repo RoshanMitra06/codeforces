5e0a1452d2851a6fcdeb9a846bf167c46d7879aa
Roshan <roosh.06@gmail.com>
2020-11-08 11:03:00 +0530

    Add solution for problem `B - Gifts Fixing` Link: https://codeforces.com/contest/1399/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1399/submission/97893407

---
+++ codeforces/1399/B.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//keep coding
+//-Roshan Mitra (SIT Pune) 
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define f0(i,n) for(int i=0;i<n;i++) 
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define int long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+signed main()
+{
+
+FIO;
+int t;
+cin>>t;
+while(t--){
+ int n;
+ cin>>n;
+ vi a(n),b(n);
+ f0(i,n)
+    cin>>a[i];
+f0(i,n)
+    cin>>b[i];
+int mina=*min_element(all(a));
+int minb=*min_element(all(b));
+f0(i,n){
+    a[i]=a[i]-mina;
+    b[i]=b[i]-minb;
+}
+int ans=0;
+f0(i,n){
+    ans+=max(a[i],b[i]);
+}
+cout<<ans<<endl;
+ 
+} 
+return 0;
+   
+}
