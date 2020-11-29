be3ee59a8e542f95698544295928e96456cca3ac
Roshan <roosh.06@gmail.com>
2020-11-29 23:25:00 +0530

    Add solution for problem `A - Magical Sticks` Link: https://codeforces.com/contest/1371/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1371/submission/99929946

---
+++ codeforces/1371/A.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//keep coding
+//-Roshan Mitra (SIT Pune) 
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define f0(i,n) for(int i=0;i<n;i++) 
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define int long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+signed main()
+{
+ //OJ;
+ FIO;
+int t;
+cin>>t;
+while(t--){
+ int n;
+ cin>>n;
+ if(n==1) cout<<1<<endl;
+ else{
+    if(n&1) cout<<(n/2)+1<<endl;
+    else cout<<n/2<<endl;
+ }
+} 
+return 0;
+   
+}
