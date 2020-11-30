8dd5e7d54860bc2bcfc21e1911c7d2448d8f9c71
Roshan <roosh.06@gmail.com>
2020-11-30 21:26:00 +0530

    Add solution for problem `B - Jumps` Link: https://codeforces.com/contest/1455/problem/B Tags: constructive algorithms, math, *1200 Ref: https://codeforces.com/contest/1455/submission/100035803

---
+++ codeforces/1455/B.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//keep coding
+//-Roshan Mitra (SIT Pune) 
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define f0(i,n) for(int i=0;i<n;i++) 
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define int long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+signed main()
+{
+
+ FIO;
+int t;
+cin>>t;
+while(t--){
+int n;
+cin>>n;
+int count=0,k=1,sum=0;
+while(sum<n){
+    sum+=k;
+    count++;
+    k++;
+}
+if(sum-n==0) cout<<count<<endl;
+else if(sum-n==1) cout<<count+1<<endl;
+else cout<<count<<endl;
+} 
+return 0;
+   //pehle piche jake aage benifit
+}
