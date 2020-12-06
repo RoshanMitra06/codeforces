fde6f0306d22f9c93d9e8e41ee8f733da1c087e1
Roshan <roosh.06@gmail.com>
2020-12-06 12:22:00 +0530

    Add solution for problem `B - Ternary Sequence` Link: https://codeforces.com/contest/1401/problem/B Tags: constructive algorithms, greedy, math, *1100 Ref: https://codeforces.com/contest/1401/submission/100496582

---
+++ codeforces/1401/B.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//keep coding
+//-Roshan Mitra (SIT Pune) 
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define f0(i,n) for(int i=0;i<n;i++) 
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define int long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+signed main()
+{
+ //OJ;
+ FIO;
+int t;
+cin>>t;
+while(t--){
+ int a,b,c,x,y,z;
+ cin>>a>>b>>c>>x>>y>>z;
+ int tot=2*min(y,c);
+ c-=min(y,c);
+ y-=min(y,c);
+
+ z-=min(c,z);
+ //cout<<z<<endl;
+ z-=min(a,z);
+ tot-=2*z;
+ cout<<tot<<endl;
+ 
+} 
+return 0;
+   
+}
