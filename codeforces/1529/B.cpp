0bea56fcce7712ca3ffae24baac6f209ae2be836
Roshan <roosh.06@gmail.com>
2021-05-26 21:39:00 +0530

    Add solution for problem `B - Sifid and Strange Subsequences` Link: https://codeforces.com/contest/1529/problem/B Tags: greedy, math, sortings, *1100 Ref: https://codeforces.com/contest/1529/submission/117426745

---
+++ codeforces/1529/B.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Athor : Roshan Mitra 😎
+// Language: C++
+// College: Symbiosis Institute Of Technology
+// Practice is the only key to success
+// Always push ur limits
+typedef pair<int, int> pii;
+#define endl "\n"
+#define set_bits(a) __builtin_popcountll(a)
+#define f0(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define sd(val) scanf("%d", &val)
+#define sl(val) scanf("%lld", &val)
+#define debug(val) printf("check%d\n", val)
+#define asc(v) v.begin(), v.end()
+#define des(v) v.end(), v.begin()
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define int long long
+#define maxheap priority_queue<int>
+#define minheap priority_queue<int, vector<int>, greater<int>>
+typedef vector<int> vi;
+#define MOD 1000000007
+#define clr(val) memset(val, 0, sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define rr return
+#define FIO                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL);
+void file()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+  vi v(n), ne, p;
+  f0(i, n)
+  {
+    cin >> v[i];
+
+    if (v[i] <= 0)
+      ne.pb(v[i]);
+    else
+      p.pb(v[i]);
+  }
+
+  sort(all(ne));
+  if (p.size() == 0)
+  {
+    cout << ne.size() << endl;
+    rr;
+  }
+  int mini = *min_element(all(p));
+  for (int i = 1; i < ne.size(); i++)
+  {
+    if (abs(ne[i - 1] - ne[i]) < mini)
+    {
+      cout << ne.size() << endl;
+      rr;
+    }
+  }
+  cout << ne.size() + 1 << endl;
+}
+signed main()
+{
+  file();
+  FIO;
+  int t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
